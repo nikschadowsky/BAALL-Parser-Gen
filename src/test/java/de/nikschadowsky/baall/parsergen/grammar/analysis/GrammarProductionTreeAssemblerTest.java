a64f85980b481755f4dd404aae7b8d5fbca28c24
nikschadowsky <n.s-schadowsky@t-online.de>
2024-03-15 21:38:35 +0100

    test: add test class for tree generation

---
+++ src/test/java/de/nikschadowsky/baall/parsergen/grammar/analysis/GrammarProductionTreeAssemblerTest.java
@@ -0,0 +1,15 @@
+package de.nikschadowsky.baall.parsergen.grammar.analysis;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * File created on 15.03.2024
+ */
+class GrammarProductionTreeAssemblerTest {
+
+    @Test
+    void generateConditionTree() {
+    }
+}
