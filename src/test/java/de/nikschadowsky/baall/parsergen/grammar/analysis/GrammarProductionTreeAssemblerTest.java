4627ed3fb42bf769ab10da7e3b3d5b8c58101476
nikschadowsky <n.s-schadowsky@t-online.de>
2024-03-21 18:40:06 +0100

    test: add test coverage for tree generation

--- src/test/java/de/nikschadowsky/baall/parsergen/grammar/analysis/GrammarProductionTreeAssemblerTest.java
+++ src/test/java/de/nikschadowsky/baall/parsergen/grammar/analysis/GrammarProductionTreeAssemblerTest.java
@@ -1,15 +1,113 @@
 package de.nikschadowsky.baall.parsergen.grammar.analysis;
 
-import org.junit.jupiter.api.Test;
+import de.nikschadowsky.baall.parsergen._utility.GrammarUtility;
+import de.nikschadowsky.baall.parsergen.grammar.GrammarNonterminal;
+import de.nikschadowsky.baall.parsergen.grammar.GrammarProduction;
+import de.nikschadowsky.baall.parsergen.grammar.GrammarTerminal;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * File created on 15.03.2024
  */
 class GrammarProductionTreeAssemblerTest {
 
-    @Test
-    void generateConditionTree() {
+    @ParameterizedTest
+    @MethodSource("generateConditionTestProvider")
+    void generateConditionTree(List<GrammarProduction> ruleset, GrammarProductionTreeNode.Root expected) {
+        GrammarNonterminal nonterminal = new GrammarNonterminal("TEST");
+        nonterminal.addProductionRules(ruleset);
+
+        assertEquals(expected, GrammarProductionTreeAssembler.generateConditionTree(nonterminal));
     }
+
+    static Stream<Arguments> generateConditionTestProvider() {
+        return Stream.of(
+                argumentsForEmpty(),
+                argumentsForLinear(),
+                argumentsForSingleBranching(),
+                argumentsForDoubleBranching()
+        );
+    }
+
+    private static Arguments argumentsForEmpty() {
+        GrammarProduction empty = new GrammarProduction(-1);
+
+        GrammarProductionTreeNode.Root expected = new GrammarProductionTreeNode.Root(true);
+
+        return Arguments.of(List.of(empty), expected);
+
+    }
+
+    private static Arguments argumentsForLinear() {
+        GrammarProduction linear = new GrammarProduction(-1,
+                GrammarUtility.getTerminalList("a", "b", "c").toArray(new GrammarTerminal[0]));
+
+        GrammarProductionTreeNode.Root expected = new GrammarProductionTreeNode.Root(false);
+
+        expected.addUniqueChild(GrammarUtility.getTerminalWithTypeAny("a"))
+                .addUniqueChild(GrammarUtility.getTerminalWithTypeAny("b"))
+                .addUniqueChild(GrammarUtility.getTerminalWithTypeAny("c"));
+
+        return Arguments.of(List.of(linear), expected);
+    }
+
+    private static Arguments argumentsForSingleBranching() {
+        GrammarTerminal first = GrammarUtility.getTerminalWithTypeAny("a");
+
+        GrammarProduction left = new GrammarProduction(-1, first, GrammarUtility.getTerminalWithTypeAny("b1"));
+        GrammarProduction middle = new GrammarProduction(-1, first, GrammarUtility.getTerminalWithTypeAny("b2"));
+        GrammarProduction right = new GrammarProduction(-1, first, GrammarUtility.getTerminalWithTypeAny("b3"));
+
+        GrammarProductionTreeNode.Root expected = new GrammarProductionTreeNode.Root(false);
+
+        GrammarProductionTreeNode.SymbolNode firstNode = expected.addUniqueChild(GrammarUtility.getTerminalWithTypeAny("a"));
+        firstNode.addUniqueChild(GrammarUtility.getTerminalWithTypeAny("b1"));
+        firstNode.addUniqueChild(GrammarUtility.getTerminalWithTypeAny("b2"));
+        firstNode.addUniqueChild(GrammarUtility.getTerminalWithTypeAny("b3"));
+
+        return Arguments.of(List.of(left, middle, right), expected);
+    }
+
+    private static Arguments argumentsForDoubleBranching() {
+        GrammarProduction left = new GrammarProduction(
+                -1,
+                GrammarUtility.getTerminalWithTypeAny("a"),
+                GrammarUtility.getTerminalWithTypeAny("b1"),
+                GrammarUtility.getTerminalWithTypeAny("c1")
+        );
+
+        GrammarProduction mid = new GrammarProduction(
+                -1,
+                GrammarUtility.getTerminalWithTypeAny("a"),
+                GrammarUtility.getTerminalWithTypeAny("b2"),
+                GrammarUtility.getTerminalWithTypeAny("c1")
+        );
+
+        GrammarProduction right = new GrammarProduction(
+                -1,
+                GrammarUtility.getTerminalWithTypeAny("a"),
+                GrammarUtility.getTerminalWithTypeAny("b2"),
+                GrammarUtility.getTerminalWithTypeAny("c2")
+        );
+
+
+        GrammarProductionTreeNode.Root expected = new GrammarProductionTreeNode.Root(false);
+        GrammarProductionTreeNode.SymbolNode a = expected.addUniqueChild(GrammarUtility.getTerminalWithTypeAny("a"));
+
+        a.addUniqueChild(GrammarUtility.getTerminalWithTypeAny("b1")).addUniqueChild(GrammarUtility.getTerminalWithTypeAny("c1"));
+
+        GrammarProductionTreeNode.SymbolNode b2 = a.addUniqueChild(GrammarUtility.getTerminalWithTypeAny("b2"));
+        b2.addUniqueChild(GrammarUtility.getTerminalWithTypeAny("c1"));
+        b2.addUniqueChild(GrammarUtility.getTerminalWithTypeAny("c2"));
+
+        return Arguments.of(List.of(left, mid, right), expected);
+    }
+
 }
