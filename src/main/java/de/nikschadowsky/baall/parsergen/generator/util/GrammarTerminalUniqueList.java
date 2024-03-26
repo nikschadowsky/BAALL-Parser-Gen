c662c9cc974816a0acdb566d184ce0971c548fdc
nikschadowsky <n.s-schadowsky@t-online.de>
2024-03-26 21:31:28 +0100

    feat: add operational methods

--- src/main/java/de/nikschadowsky/baall/parsergen/generator/util/GrammarTerminalUniqueList.java
+++ src/main/java/de/nikschadowsky/baall/parsergen/generator/util/GrammarTerminalUniqueList.java
@@ -4,15 +4,24 @@ import de.nikschadowsky.baall.parsergen.grammar.GrammarTerminal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class GrammarTerminalUniqueList {
 
     private final List<GrammarTerminal> terminalList = new ArrayList<>();
 
-    private void add(GrammarTerminal terminal) {
-        if (terminalList.stream().noneMatch(t -> t.symbolDeepEquals(terminal))){
+    public void add(GrammarTerminal terminal) {
+        if (terminalList.stream().noneMatch(t -> t.symbolDeepEquals(terminal))) {
             terminalList.add(terminal);
         }
+    }
+
+    public boolean isEmpty() {
+        return terminalList.isEmpty();
+    }
+
+    public Stream<GrammarTerminal> stream() {
+        return terminalList.stream();
     }
 
 }
