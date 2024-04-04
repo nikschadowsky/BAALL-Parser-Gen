999e02659437a957f6b2964978f5eaa337d65224
nikschadowsky <n.s-schadowsky@t-online.de>
2024-04-04 23:34:44 +0200

    refactor: rename main class

---
+++ src/main/java/de/nikschadowsky/baall/parsergen/BaallParserGenerator.java
@@ -0,0 +1,11 @@
+package de.nikschadowsky.baall.parsergen;
+
+/**
+ * File created on 24.02.2024
+ */
+public class BaallParserGenerator {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/de/nikschadowsky/baall/parsergen/ParserGenerator.java
+++
@@ -1,7 +0,0 @@
-package de.nikschadowsky.baall.parsergen;
-
-/**
- * File created on 24.02.2024
- */
-public class ParserGenerator {
-}
